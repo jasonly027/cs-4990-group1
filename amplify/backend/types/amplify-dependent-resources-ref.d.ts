8e8f70b55a2385a3df0d05a916ea729cabbd2efe
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2024-01-07 01:49:17 -0800

    Amplify w/ Storage and React

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cs4990group1c0e49957": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "videoStorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
