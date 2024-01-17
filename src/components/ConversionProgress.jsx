d40bc48500edfcdb1a5fadb32828c91a64438f11
Leaversa <mtkwong@cpp.edu>
2024-01-16 16:08:50 -0800

    fix conversion progress not displaying

--- src/components/ConversionProgress.jsx
+++ src/components/ConversionProgress.jsx
@@ -2,21 +2,16 @@ import { Progress } from "@/components/ui/progress";
 export function ConversionProgress({ currentFileState }) {
   const [currentFile] = currentFileState;
 
-  if (currentFile === null || currentFile.File === null) {
+  if (currentFile.File === null) {
     return null;
   }
-
   return (
     <>
-      {currentFile === null || currentFile.File === null ? (
-        <>
-          <h2 className="text-lg font-semibold">Conversion Progress</h2>
-          <div>
-            {/* <p className="font-medium">File1.mp4</p> */}
-            <Progress className="mt-2 h-2" value={currentFile?.progress} />
-          </div>
-        </>
-      ) : null}
+      <h2 className="text-lg font-semibold">Conversion Progress</h2>
+      <div>
+        <p className="font-medium">{currentFile.File.name}</p>
+        <Progress className="mt-2 h-2" value={currentFile?.progress} />
+      </div>
     </>
   );
 }
